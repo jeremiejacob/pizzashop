4063b6e73407587cc033eca9183e892cfa882a05
jeremie jacob <j.jacob@bizwind.co.jp>
2014-10-24 17:15:33 +0800

    Display base list screen

--- src/main/java/com/springsource/roo/pizzashop/web/BaseController.java
+++ src/main/java/com/springsource/roo/pizzashop/web/BaseController.java
@@ -1,11 +1,20 @@
 package com.springsource.roo.pizzashop.web;
-import com.springsource.roo.pizzashop.domain.Base;
-import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/bases")
+import com.springsource.roo.pizzashop.domain.Base;
+
+@RequestMapping("/bases/**")
 @Controller
-@RooWebScaffold(path = "bases", formBackingObject = Base.class)
 public class BaseController {
+	/**
+	 * Display list screen
+	 */
+	@RequestMapping(method = RequestMethod.GET, value="list")
+	public String list(Model model) {
+		model.addAttribute("bases", Base.findAllBases());
+		return "bases/list";
+	}
 }
