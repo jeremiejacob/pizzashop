701401ad85ddf1ef0b5b8f1a994651cd92274bd5
jeremie jacob <j.jacob@bizwind.co.jp>
2014-10-24 16:50:20 +0800

    Revert "Display Base list screen"
    This reverts commit 5d3fc5e1c1ee159b14982814872a0702b9276fd6.

--- src/main/java/com/springsource/roo/pizzashop/web/BaseController.java
+++ src/main/java/com/springsource/roo/pizzashop/web/BaseController.java
@@ -1,24 +1,11 @@
 package com.springsource.roo.pizzashop.web;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.springsource.roo.pizzashop.domain.Base;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/bases/**")
+@RequestMapping("/bases")
 @Controller
-//@RooWebScaffold(path = "bases", formBackingObject = Base.class)
+@RooWebScaffold(path = "bases", formBackingObject = Base.class)
 public class BaseController {
-	
-	/**
-	 * Display list screen
-	 */
-	@RequestMapping(method = RequestMethod.GET, value = "list")
-	public String list(Model model) {
-		model.addAttribute("bases", Base.findAllBases());
-		return "bases/list";
-	}
-	
 }
