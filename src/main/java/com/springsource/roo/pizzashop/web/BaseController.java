1d5369a2c7a5b85e53abfedff6ca966f4319011c
jeremie jacob <j.jacob@bizwind.co.jp>
2014-10-27 17:17:31 +0800

    First startup

--- src/main/java/com/springsource/roo/pizzashop/web/BaseController.java
+++ src/main/java/com/springsource/roo/pizzashop/web/BaseController.java
@@ -2,7 +2,6 @@ package com.springsource.roo.pizzashop.web;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.springsource.roo.pizzashop.domain.Base;
 
 @RequestMapping("/bases")
---
+++ src/test/java/com/springsource/roo/pizzashop/domain/CustomerIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.springsource.roo.pizzashop.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Customer.class)
+public class CustomerIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
