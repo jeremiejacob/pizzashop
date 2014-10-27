1a27516dc6034996cebcef3c652af44fcbe0132a
jeremie jacob <j.jacob@bizwind.co.jp>
2014-10-27 09:51:56 +0800

    Base controller unscaffolding

--- src/main/java/com/springsource/roo/pizzashop/web/BaseController.java
+++ src/main/java/com/springsource/roo/pizzashop/web/BaseController.java
@@ -7,51 +7,111 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
 import com.springsource.roo.pizzashop.domain.Base;
 
-@RequestMapping("/bases/**")
+@RequestMapping("/bases")
 @Controller
 public class BaseController {
 	/**
-	 * Display list screen
-	 */
-	@RequestMapping(method = RequestMethod.GET, value="list")
-	public String list(Model model) {
-		model.addAttribute("bases", Base.findAllBases());
-		return "bases/list";
-	}
-	
-	/**
+	 * 
 	 * Display create screen
 	 */
-	@RequestMapping(method = RequestMethod.POST, value = "create")
-	public String create(@Valid Base base, BindingResult bindingResult, Model model, HttpServletRequest httpServletRequest) {
-		if (bindingResult.hasErrors()) {
-			populateEditForm(model, base);
-			return "bases/create";
-		}
-		base.persist();
-		return "redirect:/bases/" + encodeUrlSegment(base.getId().toString(), httpServletRequest);
-	}
-	
-	void populateEditForm(Model model, Base base) {
-		model.addAttribute("base", base);
-	}
-	
-	String encodeUrlSegment(String pathSegment, HttpServletRequest httpServletRequest) {
-		String enc = httpServletRequest.getCharacterEncoding();
-		if (enc == null) {
-			enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
-		}
-		try {
-			pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
-		} catch (UnsupportedEncodingException uee) {}
-		return pathSegment;
-	}
+	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
+    public String create(@Valid Base base, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+        if (bindingResult.hasErrors()) {
+            populateEditForm(uiModel, base);
+            return "bases/create";
+        }
+        uiModel.asMap().clear();
+        base.persist();
+        return "redirect:/bases/" + encodeUrlPathSegment(base.getId().toString(), httpServletRequest);
+    }
+    
+    @RequestMapping(params = "form", produces = "text/html")
+    public String createForm(Model uiModel) {
+        populateEditForm(uiModel, new Base());
+        return "bases/create";
+    }
+    /**
+     * 
+     * Display show screen
+     */
+    @RequestMapping(value = "/{id}", produces = "text/html")
+    public String show(@PathVariable("id") Long id, Model uiModel) {
+        uiModel.addAttribute("base", Base.findBase(id));
+        uiModel.addAttribute("itemId", id);
+        return "bases/show";
+    }
+    /**
+     * 
+     * Display list screen
+     */
+    @RequestMapping(produces = "text/html")
+    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute("bases", Base.findBaseEntries(firstResult, sizeNo, sortFieldName, sortOrder));
+            float nrOfPages = (float) Base.countBases() / sizeNo;
+            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute("bases", Base.findAllBases(sortFieldName, sortOrder));
+        }
+        return "bases/list";
+    }
+    /**
+     * 
+     * Display update screen
+     */
+    @RequestMapping(method = RequestMethod.PUT, produces = "text/html")
+    public String update(@Valid Base base, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+        if (bindingResult.hasErrors()) {
+            populateEditForm(uiModel, base);
+            return "bases/update";
+        }
+        uiModel.asMap().clear();
+        base.merge();
+        return "redirect:/bases/" + encodeUrlPathSegment(base.getId().toString(), httpServletRequest);
+    }
+    
+    @RequestMapping(value = "/{id}", params = "form", produces = "text/html")
+    public String updateForm(@PathVariable("id") Long id, Model uiModel) {
+        populateEditForm(uiModel, Base.findBase(id));
+        return "bases/update";
+    }
+    /**
+     * 
+     * Delete item
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
+    public String delete(@PathVariable("id") Long id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+        Base base = Base.findBase(id);
+        base.remove();
+        uiModel.asMap().clear();
+        uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
+        uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
+        return "redirect:/bases";
+    }
+    
+    void populateEditForm(Model uiModel, Base base) {
+        uiModel.addAttribute("base", base);
+    }
+    
+    String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
+        String enc = httpServletRequest.getCharacterEncoding();
+        if (enc == null) {
+            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
+        }
+        try {
+            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
+        } catch (UnsupportedEncodingException uee) {}
+        return pathSegment;
+    }
 }
