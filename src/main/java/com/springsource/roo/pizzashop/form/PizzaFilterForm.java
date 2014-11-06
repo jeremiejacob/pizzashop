aa9dde167487505929f0aebc6e4a541ca123f0e4
jeremie jacob <j.jacob@bizwind.co.jp>
2014-11-06 18:22:08 +0800

    Create filter for pizza

--- src/main/java/com/springsource/roo/pizzashop/form/PizzaFilterForm.java
+++ src/main/java/com/springsource/roo/pizzashop/form/PizzaFilterForm.java
@@ -1,9 +1,13 @@
 package com.springsource.roo.pizzashop.form;
 
+import java.util.List;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 
 @RooJavaBean
 public class PizzaFilterForm {
 	private String name;
 	private Float price;
+	private Integer base;
+	private List<Integer>toppings;
 }
--- src/main/java/com/springsource/roo/pizzashop/service/PizzaService.java
+++ src/main/java/com/springsource/roo/pizzashop/service/PizzaService.java
@@ -1,5 +1,6 @@
 package com.springsource.roo.pizzashop.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,14 +9,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.springsource.roo.pizzashop.domain.Base;
 import com.springsource.roo.pizzashop.domain.Pizza;
+import com.springsource.roo.pizzashop.domain.Topping;
 import com.springsource.roo.pizzashop.form.PizzaFilterForm;
+import com.springsource.roo.pizzashop.web.PizzaController;
 
 @Service
 public class PizzaService {
+	private static final Logger LOGGER = Logger.getLogger(PizzaService.class);
+	
 	@PersistenceContext
 	private EntityManager entityManager;
 	
@@ -29,20 +36,57 @@ public class PizzaService {
 	}
 	
 	public List<Pizza> findAllPizzasWithCondition(PizzaFilterForm form) {
-		Map<String, Object> parameters = new HashMap<String, Object>();
+		Map<String, Object> paramValue = new HashMap<String, Object>();
+		List<String> params = new ArrayList<String>();
+		List<Topping> tops = new ArrayList<Topping>();
 		String rootQuery = "SELECT c FROM Pizza c ";
 		if (form.getName() != null ) {
-			rootQuery = rootQuery + "WHERE UPPER(c.name) LIKE UPPER(:name)";
-			parameters.put("name", "%" + form.getName() + "%");
+			params.add("UPPER(c.name) LIKE UPPER(:name)");
+			paramValue.put("name", "%" + form.getName() + "%");
 		}
 		if (form.getPrice() != null ) {
-			rootQuery = rootQuery + "c.price=:price";
+			params.add("c.price=:price");
+			paramValue.put("price", form.getPrice());
 		}
 		
-		TypedQuery<Pizza> typedQuery = entityManager.createQuery(rootQuery, Pizza.class);
-		for (String key : parameters.keySet()) {
-			typedQuery.setParameter(key, parameters.get(key));
+		if (form.getBase() != null) {
+			params.add("c.base=:base");
+			Base nbase = new Base();
+			nbase.setId(form.getBase());
+			paramValue.put("base", nbase);
 		}
+//		select distinct pizza.id, pizza.name, pizza.price from pizza left join pizza_topping on pizza.id = pizza_topping.pizza_id where pizza_topping.topping_id in (1,4);
+		if (form.getToppings() != null) {
+			params.add("c.toppings IN :toppings");
+			for (int i=0; i < form.getToppings().size(); i++) {
+				Topping ntopping = new Topping();
+				ntopping.setId(form.getToppings().get(i));
+				tops.add(ntopping);
+			}
+			paramValue.put("toppings", tops);
+		}
+		 
+		if(params.size() != 0) {
+			rootQuery = rootQuery + "WHERE ";
+			params.add(null);
+			for (int i = 0; i < params.size(); i++) {
+				if (params.get(i) != null) {
+					if (i == 0) {
+						rootQuery += params.get(i);
+					} else {
+						rootQuery += " AND " + params.get(i);
+					}
+					
+				}
+			}
+		}
+		LOGGER.info("Final Query: " + rootQuery);
+		TypedQuery<Pizza> typedQuery = entityManager.createQuery(rootQuery, Pizza.class);
+		for (String key : paramValue.keySet()) {
+			typedQuery.setParameter(key, paramValue.get(key));
+			LOGGER.info("paramValue.get(key); " + key + " : " + paramValue.get(key));
+		}
+		
 		return typedQuery.getResultList();
 	}
 	
